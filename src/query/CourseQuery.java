6dc4c93f2e10b97c62cd3cde687e309ed28295f6
Fb1107 <904011766@qq.com>
2019-05-14 15:14:31 +0800

    Update CourseQuery.java

--- src/query/CourseQuery.java
+++ src/query/CourseQuery.java
@@ -11,15 +11,15 @@ import java.util.*;
  * 
  */
 public class CourseQuery {
-	public static void test(String string) {
+	/*public static void test(String string) {
 		CourseQuery courseQuery = new CourseQuery("data/1.csv");
 	    List<Class> r = courseQuery.match(string, 5);
 	    for (Class c : r) {
 	    	System.out.println(c);
 	   	}
-	}
+	}*/
 
-	List<Class> classlist = null;
+	List<Class> classlist = new ArrayList<Class>();
 	
 	/**
 	 * 得到所有的课程
@@ -33,9 +33,13 @@ public class CourseQuery {
 	 * 读入csv格式的课程数据
 	 * @param path 课程数据路径
 	 */
-	public CourseQuery(String path) {
+	public CourseQuery() {
 		try {
-			classlist = parseCsv(path);
+			List<String> paths = Arrays.asList("ele_plan_data.csv", "speciality_data.csv", "politics_data.csv","gym_data.csv","english_data.csv",
+							 "trans_choice_data.csv","pub_choice_data.csv","liberal_computer_data.csv");
+			for (String path : paths) {
+				classlist.addAll(parseCsv("data/"+path));
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,40 +54,44 @@ public class CourseQuery {
         Collections.sort(classlist);
         
         List<Class> r = new LinkedList<Class>();
-        for (int i = 0; i < num; i++) {
-        	r.add(classlist.get(i));
+        for (Class cls : classlist) {
+        	if (cls.score == 0) break;
+        	r.add(cls);
         }
         return r;
 	}
 	
 	public static List<Class> parseCsv(String path) throws Exception {
 		List<Class> l = new ArrayList<Class>();
-		BufferedReader br = new BufferedReader(new FileReader(path));
-		String line = "";
-		while ((line = br.readLine()) != null) {
-			if (! line.isBlank()) {
-				String[] a = line.split(",");
-				if (a.length == 0) continue;
-				l.add(new Class(line));
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				if (! line.isBlank()) {
+					String[] a = line.split(",");
+					if (a.length == 0) continue;
+					l.add(new Class(line));
+				}
 			}
+			br.close();
+		} catch (Exception e) {
 		}
-		br.close();
 		return l;
 	}
 	
 	static void testPinyinConverter() {
 		PinyinConverter pc = new PinyinConverter();
-//		System.out.println(Integer.parseInt("90ED",16));
-//		System.out.println((int) '郭');
+		System.out.println(Integer.parseInt("90ED",16));
+		System.out.println((int) '郭');
 		List<String> a = pc.getAbbr("黄骏");
-		//for (String r : a) System.out.println(r);
+		for (String r : a) System.out.println(r);
 	}
 	
-	public static void main(String[] args)  {
-//		testPinyinConverter();
-//		test();
-		testInterval();
-	}
+	//public static void main(String[] args)  {
+	//	testPinyinConverter();
+		//test();
+		//testInterval();
+	//}
 
 	/**
 	 * 测试时间解析是否正确，将打印类似如下的结果：
@@ -93,7 +101,7 @@ public class CourseQuery {
 	 * 
 	 */
 	private static void testInterval() {
-		CourseQuery courseQuery = new CourseQuery("data/1.csv");
+		CourseQuery courseQuery = new CourseQuery();
 		Class c = courseQuery.getAllClasses().get(0);
 		for (Interval i : c.getIntervals()) {
 			System.out.println(i);
